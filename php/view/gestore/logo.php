319581e138e139da07273c6c8356c511dd5d18c1
nigu81 <nigu81@github.com>
2015-08-23 19:03:57 +0200

    visualizzazione pizzeria a fianco a gestore

--- php/view/gestore/logo.php
+++ php/view/gestore/logo.php
@@ -1,4 +1,13 @@
-<p>Gestione Gestore - <?= $user->getNome().' '.$user->getCognome() ?></p>
+<p>Gestione Gestore - <?= $user->getNome().' '.$user->getCognome().' pizzeria: ' ?>
+
+<?php
+$pizzerie = PizzeriaFactory::instance()->elencoPizzerie();
+$id_pizzeria = $user->getPizzeria()->getNome();
+echo $id_pizzeria;
+             
+?>  
+</p>
 <p class="logout">
     <a href="gestore?cmd=logout">Logout</a>
 </p>
+                      
