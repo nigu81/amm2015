bee1e3eab0fa8f86ada24d4c7e1394409f97a433
nigu81 <nigu81@github.com>
2015-08-24 09:34:30 +0200

    corretti link errati sulla barra laterale

--- php/view/cliente/leftBar.php
+++ php/view/cliente/leftBar.php
@@ -3,7 +3,7 @@
 <ul>
     <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="cliente<?= $vd->scriviToken('?')?>">Home</a></li>
     <li class="<?= $vd->getSottoPagina() == 'anagrafica' ? 'current_page_item' : '' ?>"><a href="cliente/anagrafica<?= $vd->scriviToken('?')?>">Anagrafica</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'lista_ordini' ? 'current_page_item' : '' ?>"><a href="cliente/lista_ordini<?= $vd->scriviToken('?')?>">Lista Ordini</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'ordine' ? 'current_page_item' : '' ?>"><a href="cliente/ordine<?= $vd->scriviToken('?')?>">Nuovo Ordine</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'lista_ordini' ? 'current_page_item' : '' ?>"><a href="cliente/ordini<?= $vd->scriviToken('?')?>">Lista Ordini</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'ordine' ? 'current_page_item' : '' ?>"><a href="cliente/crea_ordine<?= $vd->scriviToken('?')?>">Nuovo Ordine</a></li>
 
 </ul>
