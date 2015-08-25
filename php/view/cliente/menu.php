0d8d8da81f82b4b5e01f202ea0bcb7dc928d5265
nigu81 <nigu81@github.com>
2015-08-25 12:05:06 +0200

    corretto menu cliente

--- php/view/cliente/menu.php
+++ php/view/cliente/menu.php
@@ -1,8 +1,8 @@
 <ul>
     <li class="<?= $vd->getSottoPagina() == 'home' || $vd->getSottoPagina() == null ? 'current_page_item' : ''?>"><a href="cliente<?= $vd->scriviToken('?')?>">Home</a></li>
     <li class="<?= $vd->getSottoPagina() == 'anagrafica' ? 'current_page_item' : '' ?>"><a href="cliente/anagrafica<?= $vd->scriviToken('?')?>">Anagrafica</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'lista_ordini' ? 'current_page_item' : '' ?>"><a href="cliente/lista_ordini<?= $vd->scriviToken('?')?>">Lista Ordini</a></li>
-    <li class="<?= $vd->getSottoPagina() == 'ordine' ? 'current_page_item' : '' ?>"><a href="cliente/ordine<?= $vd->scriviToken('?')?>">Ordine</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'lista_ordini' ? 'current_page_item' : '' ?>"><a href="cliente/ordini<?= $vd->scriviToken('?')?>">Lista Ordini</a></li>
+    <li class="<?= $vd->getSottoPagina() == 'ordine' ? 'current_page_item' : '' ?>"><a href="cliente/crea_ordine<?= $vd->scriviToken('?')?>">Ordine</a></li>
 </ul>
 
 
